2e1562e5d2dbc2950e999590eba3776cf9019d7f
Luiz Gustavo <go459103@gmail.com>
2025-05-15 13:46:10 -0300

    Corrigindo conflitos - restaurando versão funcional

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
   /*bedroom page*/
   const bedroom_btn_nav = document.getElementById("bedroom-btn-nav");
   const bedroom_page = document.getElementById("bedroom-page-container");
-<<<<<<< HEAD
-  /*visit us page*/
-  const visit_btn_nav = document.getElementById("visit-btn");
-  const visit_page = document.getElementById("visit-page-container");
-=======
   /*collections page*/
   const collections_btn_nav = document.getElementById("collections-btn-nav");
   const collections_page = document.getElementById(
@@ -87,13 +82,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const ItemsInCart = [];
   const ItemsEl = document.getElementById("itemsQntd");
   const arrowToTop = document.getElementById("arrowToTop");
->>>>>>> ff1b683 (atualização de código e README)
 
   // Inicialização do Locomotive Scroll
   let scroll;
 
-<<<<<<< HEAD
-=======
   /*functions*/
   function refreshScroll() {
     setTimeout(() => {
@@ -279,28 +271,17 @@ document.addEventListener("DOMContentLoaded", () => {
     .addEventListener("change", updateSubtotal);
 
   /*general code*/
->>>>>>> ff1b683 (atualização de código e README)
   window.addEventListener("load", () => {
     // Inicialize o LocomotiveScroll apenas depois que o window.load for disparado
     scroll = new LocomotiveScroll({
       el: document.querySelector("[data-scroll-container]"),
       smooth: true,
-<<<<<<< HEAD
-      smartphone: { smooth: false },
-      tablet: { smooth: false },
-      inertia: 0.8,
-      lerp: 0.1,
-      multiplier: 2,
-    });
-
-=======
       lerp: 0.08,
       multiplier: 1.2,
     });
 
     let scrollPosition = 0;
 
->>>>>>> ff1b683 (atualização de código e README)
     // Correção para animações do GSAP
     scroll.on("scroll", (args) => {
       gsap.to(".specialties-container ul li", {
@@ -319,91 +300,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-<<<<<<< HEAD
-  /*living page*/
-  if (living_btn_nav && living_page && nav_items && visit_page) {
-    living_btn_nav.addEventListener("click", function () {
-      const wasVisitPageOpen = visit_page.classList.contains("show"); // Verifica se visit_page estava aberta
-
-      living_page.classList.toggle("show");
-      nav_items.classList.toggle("show");
-      /*remove*/
-      nav_items.classList.remove("show2");
-      nav_items.classList.remove("show3");
-      nav_items.classList.remove("show4");
-      visit_page.classList.remove("show");
-      dinning_page.classList.remove("show");
-      bedroom_page.classList.remove("show");
-
-      // Se a Visit Page estava aberta, reativa o scroll
-      if (wasVisitPageOpen) {
-        scroll.start();
-      }
-    });
-  }
-
-  /*dinning page*/
-  if (dinning_btn_nav && dinning_page && nav_items && visit_page) {
-    dinning_btn_nav.addEventListener("click", function () {
-      const wasVisitPageOpen = visit_page.classList.contains("show");
-      dinning_page.classList.toggle("show");
-      nav_items.classList.toggle("show3");
-      /*remove*/
-      nav_items.classList.remove("show");
-      nav_items.classList.remove("show2");
-      nav_items.classList.remove("show4");
-      living_page.classList.remove("show");
-      visit_page.classList.remove("show");
-      bedroom_page.classList.remove("show");
-
-      // Se a Visit Page estava aberta, reativa o scroll
-      if (wasVisitPageOpen) {
-        scroll.start();
-      }
-    });
-  }
-
-  if (bedroom_btn_nav && bedroom_page && nav_items && visit_page) {
-    bedroom_btn_nav.addEventListener("click", function () {
-      const wasVisitPageOpen = visit_page.classList.contains("show");
-      bedroom_page.classList.toggle("show");
-      nav_items.classList.toggle("show4");
-      /*remove*/
-      nav_items.classList.remove("show");
-      nav_items.classList.remove("show2");
-      nav_items.classList.remove("show3");
-      visit_page.classList.remove("show");
-      dinning_page.classList.remove("show");
-      living_page.classList.remove("show");
-      // Se a Visit Page estava aberta, reativa o scroll
-      if (wasVisitPageOpen) {
-        scroll.start();
-      }
-    });
-  }
-
-  if (visit_page && visit_btn_nav && nav_items && living_page) {
-    visit_btn_nav.addEventListener("click", function () {
-      visit_page.classList.toggle("show");
-      nav_items.classList.toggle("show2");
-      /*remove*/
-      nav_items.classList.remove("show");
-      nav_items.classList.remove("show3");
-      nav_items.classList.remove("show4");
-      living_page.classList.remove("show");
-      dinning_page.classList.remove("show");
-      bedroom_page.classList.remove("show");
-
-      // Se a classe "show" estiver ativa, desativa o scroll
-      if (visit_page.classList.contains("show")) {
-        scroll.stop();
-        console.log("oioi");
-        // Para o Locomotive Scroll
-      } else {
-        scroll.start(); // Ativa novamente o scroll
-      }
-    });
-=======
   const pages = {
     living: { btn: living_btn_nav, page: living_page },
     dinning: { btn: dinning_btn_nav, page: dinning_page },
@@ -859,6 +755,5 @@ document.addEventListener("DOMContentLoaded", () => {
     arrowToTop.onclick = () => {
       scroll.scrollTo(0, { duration: 3 });
     };
->>>>>>> ff1b683 (atualização de código e README)
   }
 });
